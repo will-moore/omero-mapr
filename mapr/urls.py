ffe294fceb6d9a145d8ff6b0962ab9f798b2dfa4
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-10-11 13:43:42 +0100

    fix regex and encode value

--- mapr/urls.py
+++ mapr/urls.py
@@ -35,15 +35,15 @@ reverse_lazy = lazy(reverse, str)
 # concatenate aliases to use in url regex
 MENU_MAPR_REGEX = "(%s)" % ("|".join(mapr_settings.MENU_MAPR))
 DEFAULT_MENU = mapr_settings.MENU_MAPR.iterkeys().next()
-
+VALUE_MAPR_REGEX = "[:\.\,\-\w\s]+"
 
 urlpatterns = patterns('',)
 
 # alias
 for m in mapr_settings.MENU_MAPR:
     urlpatterns += (
-        url(r'^(?i)%s/(?:(?P<value>[:\.\-\w\s]+)/)?$' % m, views.index,
-            {'menu': m},
+        url(r'^(?i)%s/(?:(?P<value>%s)/)?$' % (m, VALUE_MAPR_REGEX),
+            views.index, {'menu': m},
             name="maprindex_%s" % m),
         )
 
@@ -57,8 +57,8 @@ urlpatterns += (
             query_string=True)),
         name="maprindex"),
 
-    url(r'^api/(?P<menu>%s)/(?:(?P<value>[:\.\-\w\s]+)/)?'
-        r'count/$' % MENU_MAPR_REGEX,
+    url(r'^api/(?P<menu>%s)/(?:(?P<value>%s)/)?'
+        r'count/$' % (MENU_MAPR_REGEX, VALUE_MAPR_REGEX),
         views.api_experimenter_list,
         name='mapannotations_api_experimenters'),
     url(r'^api/(?P<menu>%s)/datasets/$' % MENU_MAPR_REGEX,
@@ -86,7 +86,8 @@ urlpatterns += (
         name='mapannotations_api_annotations'),
 
     # must be last on the list
-    url(r'^api/(?P<menu>%s)/(?:(?P<value>[:\.\-\w\s]+)/)?$' % MENU_MAPR_REGEX,
+    url(r'^api/(?P<menu>%s)/(?:(?P<value>%s)/)?$' %
+        (MENU_MAPR_REGEX, VALUE_MAPR_REGEX),
         views.api_mapannotation_list,
         name='mapannotations_api_mapannotations'),
 
