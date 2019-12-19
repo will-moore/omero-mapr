63670ee135242e447bc9aa05c0d5444f21670267
William Moore <w.moore@dundee.ac.uk>
2019-12-19 16:05:44 +0000

    py3 only

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
