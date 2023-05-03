7fb2f0f5c2ee2631cf05b53e377a74cd64de640d
William Moore <w.moore@dundee.ac.uk>
2023-05-03 16:05:05 +0100

    Add 'Publication Title' kvp above name in right panel

--- omero_mapr/templatetags/mapr_tags.py
+++ omero_mapr/templatetags/mapr_tags.py
@@ -39,3 +39,32 @@ logger = logging.getLogger(__name__)
 @register.simple_tag
 def mapr_menu_config():
     return mark_safe(json.dumps(mapr_settings.CONFIG))
+
+
+@register.inclusion_tag("mapr/includes/right_panel_title.html",
+                        takes_context=True)
+def publication_title_from_kvp(context):
+    """
+    We try to load Key-Value pairs on an object in the 'manager' and show the
+    'Publication Title' value
+    """
+
+    manager = context.get("manager", None)
+    if not hasattr(manager, "_get_object"):
+        return {}
+
+    obj = context["manager"]._get_object()
+    if obj is None:
+        return {}
+
+    title = ""
+    for ann in obj.listAnnotations():
+        if "MapAnnotationI" in str(ann.OMERO_TYPE):
+            for kvp in ann.getValue():
+                if kvp[0] == "Publication Title":
+                    title = kvp[1]
+                    break
+
+    return {
+        "title": title
+    }
